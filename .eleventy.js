71e8ab5b68e039349ac9b6b6ed7aad41d1518585
Denis Ezhkov <Denis.A.Ezhkov@rt.ru>
2020-08-27 02:09:59 +0400

    Тестовое задание

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
